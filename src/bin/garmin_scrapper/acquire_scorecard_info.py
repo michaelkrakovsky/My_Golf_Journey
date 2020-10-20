0865da989850d574a6bea1a8bae6410d4ba64a6c
Michael Krakovsky <michael_k26@hotmail.com>
2020-10-19 20:46:29 -0400

    Fix: Allowed scripts to be rexecuted with added scorecard.

--- src/bin/garmin_scrapper/acquire_scorecard_info.py
+++ src/bin/garmin_scrapper/acquire_scorecard_info.py
@@ -60,24 +60,23 @@ def login_garmin(get_scorecard_ids=False):
     Function Returns: driver (Webdriver: The webdriver to navigate throughout Selenium.)
     """
 
-    # Open session.
-    driver = webdriver.Chrome(executable_path=exe_paths['sel_driver'])
+    
+    driver = webdriver.Chrome(executable_path=exe_paths['sel_driver'])            # Open session.
     driver.get(gen_url)
-    # Switch into frame.
-    sleep(3)
+    sleep(3)                                                                      # Switch into frame.
     iframe = driver.find_element_by_xpath(frame)
     driver.switch_to.frame(iframe)
-    # Enter credentials.
-    enter_text_w_xpath(username_field, garmin_info['username'], driver)
+    enter_text_w_xpath(username_field, garmin_info['username'], driver)           # Enter credentials.
     enter_text_w_xpath(password_field, garmin_info['password'], driver)
     driver.find_element_by_xpath(submit_button).click()
 
     if get_scorecard_ids:
-        driver.get(score_url)                                  # Go to score cards.
-        source = driver.page_source                            # Dump page into file for parsing. This will contain our scorecard ids.
-        with open(source_data_location.absolute(), 'w+') as f:
+        sleep(12)
+        driver.get(score_url)                                                     # Go to score cards.
+        sleep(10)
+        source = driver.page_source                                               # Dump page into file for parsing. This will contain our scorecard ids.
+        with open(source_data_location.absolute(), 'w+', encoding='utf-8') as f:
             f.write(source)
-
     return driver
 
 def parse_score_card_ids():
@@ -89,7 +88,7 @@ def parse_score_card_ids():
     Function Returns: (List: The score card ids that are retrieved.)
     """
 
-    with open(source_data_location, 'r') as f:
+    with open(source_data_location, 'r', encoding='utf-8') as f:
         text = f.read()
     ids = findall("data-scorecard-id=\"\\d*\"", text)
     return [int(findall("\\d\\d*", id)[0]) for id in ids]
@@ -125,21 +124,20 @@ def insert_scorecard(json_text, mongo_conn):
         with open(log_file, 'w+') as f:
             f.write(str(json_text))
             raise ValueError("Unable to insert Object into Mongo DB. Check the log file at {}.".format(log_file.absolute()))
-            quit(1)
     mongo_conn.insert_one(post).inserted_id
     return True
 
-def get_scorecard_info():
+def get_scorecard_info(get_scorecard_ids):
     
     """
     Function Description: Retrieve the scorecard and game information from all the scorecard ids.
-    Function Parameters: Nothing
+    Function Parameters: get_scorecard_ids (Boolean: An indication to record the known scorecard ids.)
     Function Throws: Nothing
     Function Returns: (Boolean: True or False depending on the behavior of our script.)
     """
 
     collection = connect_to_scorecards_collection()
-    driver = login_garmin()
+    driver = login_garmin(get_scorecard_ids=get_scorecard_ids)
     sleep(10)                                                      # Wait prior to starting to parse the next score card.
     # Sample Link: https://connect.garmin.com/modern/proxy/gcs-golfcommunity/api/v2/scorecard/detail?scorecard-ids=155069236&include-next-previous-ids=true&user-locale=en
     ids = parse_score_card_ids()
@@ -159,7 +157,7 @@ def get_scorecard_info():
 
 if __name__ == "__main__":
 
-    result = get_scorecard_info()
+    result = get_scorecard_info(True)
     if result:
         print("The scorecards were retrieved from Garmin and inserted. Please check MongoDB.")
     else:
