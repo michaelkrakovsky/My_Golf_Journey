9ac5fb962a2e9fe82dc354f5089b478050016763
Michael Krakovsky <michael_k26@hotmail.com>
2020-11-24 20:11:06 -0500

    New: Graphing Putts Per Hole

--- src/bin/stat_apis/get_golf_stats.py
+++ src/bin/stat_apis/get_golf_stats.py
@@ -5,6 +5,7 @@ from sys import path
 path.extend('../../../../')                      # Import the entire project.
 from My_Golf_Journey.config import mongo_config
 from pymongo import MongoClient
+from pandas import DataFrame
 
 class Stats():
 
@@ -18,5 +19,46 @@ class Stats():
         client = MongoClient(mongo_config['conn_str'])
         db = client.Golf_Stats_DB
         self.collection = db.Scorecards
+
+    def get_putting_avg_by_hole(self, course_id):
+
+        """
+        Function Description: Get the putting average by the hole at a particular golf course.
+        Function Parameters: coourse_id (Int: The unique id to identify the golf course.)
+        Function Throws: Nothing
+        Function Returns: (DataFrame: The collection of hole numbers and putts per hole.)
+        """
+
+        df = DataFrame(list(self.collection.aggregate([
+            {"$match": {"courseSnapshots.courseGlobalId": course_id}},
+            {"$unwind": "$scorecardDetails"},
+            {"$sort": {"scorecardDetails.scorecard.startTime" : 1}},
+            {"$unwind": "$scorecardDetails.scorecard.holes"}, 
+            {"$group": {"_id": "$scorecardDetails.scorecard.holes.number",
+                "putting_average": {"$avg": "$scorecardDetails.scorecard.holes.putts"}}},
+            {'$sort': {"_id": 1}}
+        ])))
+        return df.set_index('_id')
+
+    def get_scoring_avg_by_hole(self, course_id):
+
+        """
+        Function Description: Get the scoring average by the hole at a particular golf course.
+        Function Parameters: coourse_id (Int: The unique id to identify the golf course.)
+        Function Throws: Nothing
+        Function Returns: (List: The collection of hole numbers and putts per hole.)
+        """
+
+        return list(self.collection.aggregate([
+            {"$match": {"courseSnapshots.courseGlobalId": course_id}},
+            {"$unwind": "$scorecardDetails"},
+            {"$sort": {"scorecardDetails.scorecard.startTime" : 1}},
+            {"$unwind": "$scorecardDetails.scorecard.holes"}, 
+            {"$group": {"_id": "$scorecardDetails.scorecard.holes.number",
+                "scoring_average": {"$avg": "$scorecardDetails.scorecard.holes.strokes"}}}, 
+            {'$sort': {"_id": 1}}
+        ]))
+
+
 
 tester = Stats() 
